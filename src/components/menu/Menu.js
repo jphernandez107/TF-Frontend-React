4c91bdebe920db1d6d19bb68102a5900f7742b47
juanpahernan <jphernandez107@gmail.com>
2021-10-17 12:28:06 -0300

    Deleted unused menus. Breadcrumb colors changed

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -11,37 +11,6 @@ class Menu extends React.Component {
                     <li className="nav-item">
                         <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
                     </li>
-                    <li className="nav-item d-none d-sm-inline-block">
-                        <a href="/" className="nav-link">Inicio</a>
-                    </li>
-                    <li className="nav-item d-none d-sm-inline-block">
-                        <a href="#" className="nav-link">Secciones/Filas</a>
-                    </li>
-                    <li className="nav-item d-none d-sm-inline-block">
-                        <a href="#" className="nav-link">Sectores</a>
-                    </li>
-                    <li className="nav-item d-none d-sm-inline-block">
-                        <a href="#" className="nav-link">Ayuda</a>
-                    </li>
-                </ul>
-                
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" data-widget="navbar-search" href="#" role="button">
-                            <i className="fas fa-search"></i>
-                        </a>
-                        <div className="navbar-search-block">
-                            <form className="form-inline">
-                                <div className="input-group input-group-sm">
-                                    <input className="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search"></input>
-                                    <div className="input-group-append">
-                                        <button className="btn btn-navbar" type="submit"> <i className="fas fa-search"></i> </button>
-                                        <button className="btn btn-navbar" type="button" data-widget="navbar-search"> <i className="fas fa-times"></i> </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
                 </ul>
             </nav>
    
